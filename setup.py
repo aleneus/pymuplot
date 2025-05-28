4f8c620ac8d78f3e7ab8be4e5b05f3b6571b230a
Aleksandr Popov <aleneus@bk.ru>
2025-05-28 12:15:21 +0300

    docs: license years

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2021-2022 Aleksandr Popov
+# Copyright (C) 2021-2025 Aleksandr Popov
 
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Lesser General Public License as published by
